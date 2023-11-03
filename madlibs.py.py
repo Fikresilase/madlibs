c40a00e57d45d2d7aa765d36983c1c7f7a5ddafd
Fikresilase <128931218+Fikresilase@users.noreply.github.com>
2023-11-02 23:50:50 -0700

    Add files via upload

---
+++ madlibs.py.py
@@ -0,0 +1,7 @@
+animal = input("animal: ")
+color = input("color: ")
+verb = input("verb: ")
+object = input("object: ")
+
+madlibs = f"The sun shone brightly as {animal} chirped in the clear {color} sky. A friendly dog wagged its tail while children {verb} in the park. An ice cream {object} jingled its melody, drawing people closer for sweet treats."
+print(madlibs)
